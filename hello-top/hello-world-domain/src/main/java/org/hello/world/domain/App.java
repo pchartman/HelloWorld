b993a36565af23ca3834657a333e35f9f93ea9a8
Chris Hartman <chartman@citytechinc.com>
2011-04-07 18:50:00 -0500

    Add new HelloWorld, updated old HelloWorld

--- hello-top/hello-world-domain/src/main/java/org/hello/world/domain/App.java
+++ hello-top/hello-world-domain/src/main/java/org/hello/world/domain/App.java
@@ -4,10 +4,8 @@ package org.hello.world.domain;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
